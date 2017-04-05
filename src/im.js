3ebe2b421962a4ac4e4cc32aac8869f7b75947fc
Agustín <Agustn>
2017-04-05 08:16:25 -0300

    Some changes in im.js file

--- src/im.js
+++ src/im.js
@@ -4,8 +4,28 @@ import XMPP from 'node-xmpp-client/browserify';
 export default class IM extends React.Component {
     constructor(options) {
         super();
-        let { user = 'user', password = 'secret' } = options;
-        this._connectionStatus = '';
+        this.options = Object.assign({}, options);
+
+        this.state = {
+            connectionStatus: '',
+            user: '',
+            destination: '',
+            message: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.connect = this.connect.bind(this);
+        this.sendMessage = this.sendMessage.bind(this);
+        this.destination = '';
+        this.message = '';
+    }
+
+    handleChange(event) {
+        this.setState({[event.target.name]: event.target.value});
+    }
+
+    createClient() {
+        let { user = 'user', password = 'secret' } = this.state;
+
         this.client = new XMPP.Client({
             // websocket: { url: 'ws://localhost:5280/xmpp-websocket/' },
             bosh: {url: 'http://localhost:5222/'},
@@ -15,45 +35,53 @@ export default class IM extends React.Component {
         });
 
         this.client.on('online',  () =>{
-            this.connectionStatus = 'online';
-            console.log('online');
-            var recipients = ['tres.uno@localhost'];
-            recipients.forEach(to => {
-                var stanza = new XMPP.Element('message', {to: to, type: 'chat'})
-                .c('body')
-                .t('Hello from browser');
-                this.client.send(stanza);
-            });
-                
+            this.setState({connectionStatus: 'online'});
+           
             //this.client.connection.socket.setTimeout(0);
             //this.client.connection.socket.setKeepAlive(true, 10000);
         })
 
         this.client.on('error', err => {
-            this.connectionStatus = err;
-            console.error(err);
+            this.setState({connectionStatus: err});
         })
         this.client.on('stanza', stanza => {
-            console.log('client1: stanza', stanza.root().toString());
+            this.setState({connectionStatus: stanza.toString()});
         })
-        this.connectionStatus = 'New';
-    }
-    set connectionStatus(value) { 
-        this._connectionStatus = value;
-    }
-    get connectionStatus() {
-        return this._connectionStatus;
+
+        this.client.on('disconect', () => {
+            this.setState({connectionStatus: 'disconected'});
+        })
     }
 
+    sendMessage() {
+        var recipients = [this.state.destination];
+        recipients.forEach(to => {
+            var stanza = new XMPP.Element('message', {to: to, type: 'chat'})
+            .c('body')
+            .t(this.state.message);
+            this.client.send(stanza);
+        });
+    }
+
+    connect() {
+        this.createClient();
+    }
 
     render() {
         return (
         <div className="im-container">
             <div className="im-header">
             IM
+            <input type="text" name='user' value={this.state.user} onChange={this.handleChange} />
+            <input type='button' value='Connect' onClick={this.connect}/>
             </div>
+            <label>To:</label>
+            <input name='destination' value={this.state.destination} onChange={this.handleChange} /><br/>
+            <label>Message:</label>
+            <textarea name='message' cols='40' rows='4' value={this.state.message} onChange={this.handleChange} ></textarea><br/>
+            <input type='button' value='Send' onClick={this.sendMessage}/>
             <p className="im-body">
-            <span>{this.connectionStatus}</span>
+            <span>{this.state.connectionStatus}</span>
             </p>
         </div>
         );
